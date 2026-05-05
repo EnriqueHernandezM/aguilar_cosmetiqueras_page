a57b0cab433c842b81f853069df0ef2f8a6ced95
Enrique Hernandez Montiel <enriquehmsb@gmail.comgit congig --global -egit config --global user.email enriquehmsb@gmail.com>
2026-05-05 00:34:19 -0600

    seo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/aguilar_cosmetiqueras_page/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
